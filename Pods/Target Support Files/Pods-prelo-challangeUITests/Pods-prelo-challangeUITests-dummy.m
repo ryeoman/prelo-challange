f80aeb5f2d38d0086b5d7698e7ed5f4deb85d5e4
ryeoman91 <riyo.nn@gmail.com>
2017-12-30 00:34:54 +0700

    Initial Commit

---
+++ Pods/Target Support Files/Pods-prelo-challangeUITests/Pods-prelo-challangeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_prelo_challangeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_prelo_challangeUITests
+@end
---
+++ prelo-challangeTests/prelo_challangeTests.swift
@@ -0,0 +1,36 @@
+//
+//  prelo_challangeTests.swift
+//  prelo-challangeTests
+//
+//  Created by Homade on 12/29/17.
+//  Copyright © 2017 -. All rights reserved.
+//
+
+import XCTest
+@testable import prelo_challange
+
+class prelo_challangeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
