df071aaa9e42df79a8caaf7d1d27ebddfc4bd72a
ryeoman91 <riyo.nn@gmail.com>
2017-12-30 01:32:30 +0700

    Add Login model

---
+++ prelo-challange/Model/Login.swift
@@ -0,0 +1,17 @@
+//
+//  Login.swift
+//  prelo-challange
+//
+//  Created by Homade on 12/30/17.
+//  Copyright © 2017 -. All rights reserved.
+//
+
+import Foundation
+
+struct Login: Decodable{
+    var _id: String
+    var username: String
+    var email: String
+    var fullname: String
+    var token: String
+}
